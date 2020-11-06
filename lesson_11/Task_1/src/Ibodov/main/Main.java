e1a417c4ce70f11554b3a448e254239175bc2274
Parviz Ibodov <ibodovparviz@gmail.com>
2020-11-06 12:20:30 +0500

    solved the problem №1

---
+++ lesson_11/Task_1/src/Ibodov/main/Main.java
@@ -0,0 +1,23 @@
+package Ibodov.main;
+
+import Ibodov.another.MaxSum;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        //Вывод B:
+        MaxSum.out();
+
+        // Вывод C:
+        MaxSum.sum();
+
+        // Вывод D:
+        System.out.println("Наибольшое число: " + MaxSum.max());
+
+        //Вывод D(Вариант 2):
+        MaxSum.exmsum();
+
+    }
+}
